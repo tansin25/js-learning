84a08f14140a86944ae8e29a841603b85e68ae2e
Tanmay Singh <98298849+tansin25@users.noreply.github.com>
2024-05-24 10:16:27 +0000

    datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail="xx@gmail.com"
+var accopuntPassword="12345"
+accountCity="Jaipur "
+// accountId=2 //not allowed
+accountEmail="rer@gmail.com"
+accountPassword="35etert"
+accountCity="gfgfg"
+
+console.log(accountId);
+/*DO NOT USE VAR FOR VARIABLE DECL. DUE TO ISSUE IN BLOCK SCOPE AND FUNCTIONAL SCOPE  */
+
+
+console.table([accountId,accountEmail,accountCity,accopuntPassword])
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //it treats all js code as new version
+
+// alert(3+3) //cant run on node js
+
+let name="tan" //string
+let age=344 //number
+let isLoggedIn = false //boolean
+
+//null -> standalone value 
+//undefined -> no value is defined 
+//symbol-> used for uniqueness
+
+//object->
+
+console.log(typeof null)
